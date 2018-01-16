b9109feda8e8d001d989e2a333462ecaa292f637
Demetrius Albuquerque <causbla@gmail.com>
2018-01-16 16:42:41 -0400

    added navigation to product detail

--- APM-Start/src/app/product-list/product-detail.component.ts
+++ APM-Start/src/app/product-list/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { IProduct } from './product';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
-  constructor() { }
+  constructor(private _activeRouter: ActivatedRoute, private _router: Router) { }
 
   ngOnInit() {
+    const id = +this._activeRouter.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Video Game Controller',
+      'productCode': 'GMG-0042',
+      'releaseDate': 'October 15, 2015',
+      'description': 'Standard two-button video game controller',
+      'price': 35.95,
+      'starRating': 4.6,
+      'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png'
+    };
+  }
+
+  onBack(): void {
+    this._router.navigate(['/products']);
   }
 
 }
