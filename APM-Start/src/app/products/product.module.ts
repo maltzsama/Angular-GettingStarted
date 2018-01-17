ab1f2f72158693c920b3b06dee5330960be5e712
Demetrius Albuquerque <causbla@gmail.com>
2018-01-16 22:29:20 -0400

    shared module added

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,28 +1,24 @@
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { StarComponent } from './../shared/star/star.component';
 import { ConvertToSpacesPipe } from './../shared/convert-to-space.pipe';
 
 import { ProductGuardService } from './product-guard.service';
 import { ROUTES_PRODUCTS } from './product.routes';
 import { ProductsService } from './products.service';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild(ROUTES_PRODUCTS)
+    RouterModule.forChild(ROUTES_PRODUCTS),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpacesPipe,
-    StarComponent
   ],
   providers: [
     ProductsService,
